926c6b747efa6df884923fc5151d270b2b2d541d
asifjoardar <mdasifjoardar@gmail.com>
2023-07-10 15:36:55 +0600

    login registration form added

--- src/main/java/com/asif/meternotifier/controller/HomeController.java
+++ src/main/java/com/asif/meternotifier/controller/HomeController.java
@@ -6,13 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String home(){
-        return "index";
+    public String login(){
+        return "login";
     }
 
     @GetMapping("/registration")
     public String registration(){
         return "registration";
     }
-
 }
