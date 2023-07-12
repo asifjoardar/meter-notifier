1a1738bfdd2e6f45c3bc9ca5173ac944a35966c6
asifjoardar <mdasifjoardar@gmail.com>
2023-07-12 23:32:21 +0600

    refector some code

--- src/main/java/com/asif/meternotifier/util/NotificationScheduler.java
+++ src/main/java/com/asif/meternotifier/util/NotificationScheduler.java
@@ -10,7 +10,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
 import java.util.List;
 
 @Service
@@ -19,31 +18,40 @@ public class NotificationScheduler {
     private RequestSender requestSender;
     private EmailSender emailSender;
 
-
     public NotificationScheduler(MeterAccountDetailsRepository meterAccountDetailsRepository, RequestSender requestSender, EmailSender emailSender){
         this.meterAccountDetailsRepository = meterAccountDetailsRepository;
         this.requestSender = requestSender;
         this.emailSender = emailSender;
     }
     Logger logger = LoggerFactory.getLogger(MeterAccountDetails.class);
-    @Scheduled(fixedRate = 3600000)
+    @Scheduled(fixedRate = 2000)
     public void execute() throws JsonProcessingException {
-        System.out.println("scheduler call in " + new Date().toString());
-
-        List<MeterAccountDetails> meterAccountDetailsList = meterAccountDetailsRepository.findAllByNotification(true);
-        for (MeterAccountDetails meterAccountDetails:meterAccountDetailsList){
-            String url = "http://prepaid.desco.org.bd/api/tkdes/customer/getBalance?accountNo="+
-                            meterAccountDetails.getAccountNumber()+
-                            "&meterNo="+
-                            meterAccountDetails.getMeterNumber();
-            ObjectMapper mapper = new ObjectMapper();
-            Data data = mapper.treeToValue(requestSender.request(url), Data.class);
+        List<MeterAccountDetails> meterAccountDetailsListTrue = meterAccountDetailsRepository.findAllByNotification(true);
+        List<MeterAccountDetails> meterAccountDetailsListFalse = meterAccountDetailsRepository.findAllByNotification(false);
+        for (MeterAccountDetails meterAccountDetails:meterAccountDetailsListFalse){
+            String url = urlMaker(meterAccountDetails.getAccountNumber(), meterAccountDetails.getMeterNumber());
+            Data data = getDataFromMapper(url);
+            if(data.getBalance() > 1500.00 && meterAccountDetails.isNotified() == true){
+                meterAccountDetails.setNotified(false);
+                meterAccountDetailsRepository.save(meterAccountDetails);
+            }
+        }
+        for (MeterAccountDetails meterAccountDetails:meterAccountDetailsListTrue){
+            String url = urlMaker(meterAccountDetails.getAccountNumber(), meterAccountDetails.getMeterNumber());
+            Data data = getDataFromMapper(url);
             if(data.getBalance() <= 1500.00 && meterAccountDetails.isNotified() == false){
-                emailSender.send(meterAccountDetails.getCustomer().getEmail(), "Balance is low", "Dear Customer your current balance is "+data.getBalance()+"taka please recharge.");
+                emailSender.send(meterAccountDetails.getCustomer().getEmail(), "Balance is low", "Dear Customer your current balance is "+data.getBalance()+" taka please recharge.");
                 meterAccountDetails.setNotified(true);
                 meterAccountDetailsRepository.save(meterAccountDetails);
             }
             logger.info("logging date: " + meterAccountDetails);
         }
+    }
+    public String urlMaker(String acNo, String meterNo){
+        return "http://prepaid.desco.org.bd/api/tkdes/customer/getBalance?accountNo="+ acNo+ "&meterNo="+ meterNo;
+    }
+    public Data getDataFromMapper(String url) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.treeToValue(this.requestSender.request(url), Data.class);
     }
 }
