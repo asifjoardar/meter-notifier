d3d33a837a8fe5ffeed6a3bf36751b7bf2105920
asifjoardar <mdasifjoardar@gmail.com>
2023-07-27 12:22:46 +0600

    reform data mapper

--- src/main/java/com/asif/meternotifier/util/DataMapperUtil.java
+++ src/main/java/com/asif/meternotifier/util/DataMapperUtil.java
@@ -4,28 +4,37 @@ import com.asif.meternotifier.dto.Data;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
 public class DataMapperUtil {
-    private final UrlMakerUtil urlMakerUtil;
     private final RestTemplate restTemplate;
 
-    public DataMapperUtil(UrlMakerUtil urlMakerUtil, RestTemplateBuilder restTemplateBuilder) {
-        this.urlMakerUtil = urlMakerUtil;
+    @Value("${data_host}")
+    private String dataHost;
+
+    public DataMapperUtil(RestTemplateBuilder restTemplateBuilder) {
         this.restTemplate = restTemplateBuilder.build();
     }
 
     public Data getDataFromMapper(String acNo, String meterNo) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
-        String url = urlMakerUtil.getUrl(acNo, meterNo);
+        String url = getUrl(acNo, meterNo);
         return mapper.treeToValue(request(url), Data.class);
     }
 
     private JsonNode request(String url) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         return mapper.readTree(restTemplate.getForObject(url, String.class)).path("data");
+    }
+    private String getUrl(String acNo, String meterNo) {
+        return dataHost
+                + "/api/tkdes/customer/getBalance?accountNo="
+                + acNo
+                + "&meterNo="
+                + meterNo;
     }
 }
